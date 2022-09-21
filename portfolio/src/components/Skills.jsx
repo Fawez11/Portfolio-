ba1eb022f893b13bb90867a1d29ac5c06e3a0172
Fawez11 <fawezferjaoui11@gmail.com>
2022-09-21 15:27:04 +0200

    finishing with skills

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -5,7 +5,7 @@ import CSS from '../assets/css.png';
 import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
-import FireBase from '../assets/firebase.png';
+import fireBase from '../assets/firebase.png';
 import Vue from '../assets/Vue.png';
 import GitHub from '../assets/github.png';
 import Mysql from '../assets/Mysql.png';
@@ -14,6 +14,8 @@ import Nuxt from '../assets/Nuxt.png';
 import Next from '../assets/Next.png';
 import Typescript from '../assets/Typescript.png';
 import Tailwind from '../assets/Tailwind.png'
+import Reactnative from '../assets/Reactnative.png';
+import express from '../assets/express.png';
 const Skills = () => {
   return (
     <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
@@ -42,6 +44,10 @@ const Skills = () => {
                   <p className='my-4'>TYPESCRIPT</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Reactnative} alt="HTML icon" />
+                  <p className='my-4'>REACT-NATIVE</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={ReactImg} alt="HTML icon" />
                   <p className='my-4'>REACT</p>
               </div>
@@ -66,6 +72,10 @@ const Skills = () => {
                   <p className='my-4'>NODE JS</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={express} alt="HTML icon" />
+                  <p className='my-4'>EXPRSS JS </p>
+              </div>
+              {/* <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
                   <p className='my-4'>TAILWINDCSS</p>
               </div>
@@ -77,6 +87,10 @@ const Skills = () => {
                   <img className='w-20 mx-auto' src={Mysql} alt="HTML icon" />
                   <p className='my-4'>MYSQL</p>
               </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={fireBase} alt="HTML icon" />
+                  <p className='my-4'>FIREBASE</p>
+              </div> */}
              
              
              

2d98effb1ffea9bee242ebb84e72167a9dc9892d
Fawez11 <fawezferjaoui11@gmail.com>
2022-09-21 16:33:49 +0200

    refactoring home page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,19 +3,18 @@ import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
+import Work from "./components/Work";
 
-
-
-// hello
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
+      <Navbar />
+      <Home />
       <About />
-     <Skills/>
-     <Contact/>
+      <Skills />
+      <Work />
+      <Contact />
     </div>
   );
 }

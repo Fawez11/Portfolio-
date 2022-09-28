c49d153eb39577169efe376d811495d769dd965f
Fawez11 <fawezferjaoui11@gmail.com>
2022-09-28 22:10:53 +0200

    finish

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import WorkImg from '../assets/workImg.jpeg';
 import realEstate from '../assets/realestate.jpg';
 import baladia from '../assets/baladia.png';
+import djerba from '../assets/djerba.jpg';
 
 const Work = () => {
   return (
@@ -65,29 +66,29 @@ const Work = () => {
             </div>
           </div>
             {/* Grid Item */}
-          {/* <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+           <div
+            style={{ backgroundImage: `url(${djerba})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          > */}
+          > 
             {/* Hover Effects */}
-            {/* <div className='opacity-0 group-hover:opacity-100'>
+             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
                 React JS Application
               </span>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://www.youtube.com/watch?v=6ypMYk7edd8&t=27s'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/Fawez11/Hung-out'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
                 </a>
               </div>
             </div>
-          </div> */}
+          </div> 
           {/* <div
             style={{ backgroundImage: `url(${realEstate})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
@@ -134,11 +135,11 @@ const Work = () => {
                 </a>
               </div>
             </div>
-          </div> */}
-          {/* <div
+          </div> 
+           <div
             style={{ backgroundImage: `url(${realEstate})` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-          > */}
+          > 
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>

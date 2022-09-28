b5769a6f9de41b891029d8e8c81ce52e43d89edf
Fawez11 <fawezferjaoui11@gmail.com>
2022-09-28 22:13:37 +0200

    finish it

--- portfolio/src/components/Work.jsx
+++ portfolio/src/components/Work.jsx
@@ -73,7 +73,7 @@ const Work = () => {
             {/* Hover Effects */}
              <div className='opacity-0 group-hover:opacity-100'>
               <span className='text-2xl font-bold text-white tracking-wider'>
-                React JS Application
+                React js Application
               </span>
               <div className='pt-8 text-center'>
                 <a href='https://www.youtube.com/watch?v=6ypMYk7edd8&t=27s'>
